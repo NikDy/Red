f72fcd6baa7138c4277bc63da8f5f2bbe5dab9d5
NikDy <nikita.dyrkov@gmail.com>
2019-11-22 15:41:34 +0300

    login

--- src/App.cpp
+++ src/App.cpp
@@ -8,24 +8,19 @@
 
 int main(int argc, char* argv[])
 {
-
-	Data_manager dat;
-	dat.login("kis");
-
-	Drawer drawer = Drawer(800, 600, "Drawer");
-	drawer.graphToShapes(dat.getMapLayer0(), dat.getMapLayer1());
-	drawer.drawAll();
-
-	/*if (argc == 2)
+	if (argc == 2)
 	{
+		Data_manager dat;
+		dat.login(argv[1]);
+
 		Drawer drawer = Drawer(800, 600, "Drawer");
-		//drawer.graphToShapes(*ptr);
+		drawer.graphToShapes(dat.getMapLayer0(), dat.getMapLayer1());
 		drawer.drawAll();
 	}
 	else 
 	{
 		std::cout << "Usage: Graph_drawer <path_to_graph.json>" << std::endl;
-	}*/
+	}
 
 	return 0;
 }
