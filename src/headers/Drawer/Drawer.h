0291b4731e6fae56553255be47c175b4a20e1a93
NikDy <nikita.dyrkov@gmail.com>
2019-12-04 00:53:02 +0300

    add drawe container

--- src/headers/Drawer/Drawer.h
+++ src/headers/Drawer/Drawer.h
@@ -7,66 +7,74 @@
 #include <mutex>
 #include <thread>
 #include <chrono>
+#include <Windows.h>
+
 
 #include "Line.h"
 #include "Forces.h"
 #include "Data_manager.h"
+#include "DrawerContainer.h"
+
 
 class Drawer
 {
 private:
-	std::map<int, sf::CircleShape> map_to_draw;
-	std::map<int, sf::Sprite> posts_to_draw;
-	std::map<int, sf::Sprite> trains_to_draw;
-	std::vector<selbaward::Line> lines_to_draw;
-	std::vector<sf::Text> text_lay0_to_draw;
-	std::vector<sf::Text> text_lay1_to_draw;
-	float w_sizeX = 800.f;
-	float w_sizeY = 600.f;
-	std::string w_name = "Drawer window";
+
+	std::map<int, std::shared_ptr<sf::Drawable>> points;
+	std::vector<selbaward::Line> lines;
+
+	float w_sizeX = std::stof(Data_manager::getInstance().config["window_W"]);
+	float w_sizeY = std::stof(Data_manager::getInstance().config["window_H"]);
+	std::string w_name = Data_manager::getInstance().config["window_title"];
+
+
 	sf::Color w_background_color = sf::Color::White;
 	sf::Color w_outline_color = sf::Color::Black;
-	sf::Color w_shapes_color = sf::Color::White;
+	sf::Color w_points_color = sf::Color::White;
 	sf::Color w_town_color = sf::Color(119, 136, 153);
 	sf::Color w_market_color = sf::Color(154, 205, 50);
 	sf::Color w_storage_color = sf::Color(189, 183, 107);
+
+
 	int w_shape_scale = 2;
 	sf::Font font;
-	sf::VertexArray lines;
 	sf::Texture market_texture;
 	sf::Texture storage_texture;
 	sf::Texture town_texture;
 	sf::Texture train_texture;
 
 	int grid_size = 0;
-	int rad = 135;
-	MapLayer1 layer1 = MapLayer1(-1);
-	Graph graph = Graph("", -1);
-	const float lines_thickness = 4.f;
-	const float camera_movement_speed = 1.5f;
-	const float camera_zoom_speed = 0.003f;
+	float points_radius = std::stof(Data_manager::getInstance().config["points_radius"]);
+	const float lines_thickness = std::stof(Data_manager::getInstance().config["lines_thickness"]);
+	const float camera_movement_speed = std::stof(Data_manager::getInstance().config["camera_move_speed"]);
+	const float camera_zoom_speed = std::stof(Data_manager::getInstance().config["camera_zoom_speed"]);
 	
-	bool marketsToShapes(std::map<int, std::shared_ptr<Market>>& markets, std::vector<sf::Text>& text_lay1_to_draw, std::map<int, sf::Sprite>& posts_to_draw);
-	bool townsToShapes(std::map<int, std::shared_ptr<Town>>& towns, std::vector<sf::Text>& text_lay1_to_draw, std::map<int, sf::Sprite>& posts_to_draw);
-	bool storagesToShapes(std::map<int, std::shared_ptr<Storage>>& storages, std::vector<sf::Text>& text_lay1_to_draw, std::map<int, sf::Sprite>& posts_to_draw);
-	bool trainsToShapes(std::map<int, Train>&  trains, std::map<int, sf::Sprite>& trains_to_draw);
 	void reforceGraph();
-	void recalcLinesShapes();
-	bool updateDataShapes();
+	void drawAll();
+	void updateShapes();
+	void buildVisualGraph();
+	void updateData();
 	
 	bool update_window = true;
 	std::thread updateThread;
 	std::thread windowThread;
-	void updateShapes();
 	std::mutex update_mutex;
 	std::condition_variable update_check;
-public:
-	Drawer(float w_sizeX_, float w_sizeY_, std::string w_name_, sf::Color w_background_color_, sf::Color w_shapes_color_);
-	Drawer(float w_sizeX_, float w_sizeY_, std::string w_name_);
+
+
+
+	Drawer() {};
 	~Drawer();
-	bool update(MapLayer1 mapLayer1);
-	bool graphToShapes(Graph graph, MapLayer1 mapLayer1);
-	void drawAll();
+public:
+	static Drawer& getInstance()
+	{
+		static Drawer instance;
+		return instance;
+	}
+	Drawer(Data_manager const&) = delete;
+	Drawer operator=(Drawer const&) = delete;
+
+	void startDraw();
 
 	bool update_on = true;
 };
