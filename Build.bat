b34d3615bea67b4bf323894c27f86e1142ee796b
NikDy <nikita.dyrkov@gmail.com>
2019-12-20 01:10:03 +0300

    build changes

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 -S ./src -B ./build
+cmake -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -S ./src -B ./build
 cmake --build ./build --config Release
 copy .\res .\build\bin
 pause
