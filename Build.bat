f8c32a9867337e929b1078a7a3db20e8ccba76b2
NikDy <nikita.dyrkov@gmail.com>
2019-12-20 03:29:02 +0300

    build fixfix

--- Build.bat
+++ Build.bat
@@ -1,3 +1,6 @@
+conan remote add sfml:bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan install ./src -if ./build --build missing
+
 cmake -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 -S ./src -B ./build
 cmake --build ./build --config Release
 copy .\res .\build\bin
